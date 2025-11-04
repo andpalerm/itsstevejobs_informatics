424841e1f4d9e1ae62c5543f08e3b8a83adb974c
Andrea Palermo <71507944+andpalerm@users.noreply.github.com>
2025-11-04 21:49:05 +0100

    Add files via upload

---
+++ EX001_SommaDegliElementi_AndreaPalermo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define N 10
+int main()
+{
+  int v[N];
+  int somma;
+  int i;
+  
+  printf("--- Inserisci %d numeri ---\n", N);
+  for (i = 0; i < N; i++) {
+    printf("Inserisci il %d° numero:\n", i+1);
+    scanf("%d", &v[i]);
+  }
+  
+  for(i = 0; i < N; i++) {
+    somma += v[i];
+    }
+    printf("\n\nLa somma totale dei numeri nell'array è: %d", somma);
+}
